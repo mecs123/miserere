d3a3b0d0cb65f9d1494336f0238c59557ed939b9
mecs123 <manolo.9315@hotmail.com>
2022-03-11 18:03:19 -0500

    Diseño del HomePrincipal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
+import { VideoComponent } from './headers/video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    PortafolioComponent,
+    TestimonialComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
